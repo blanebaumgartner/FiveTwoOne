a7207dc620d176ce1fbf19a7bb3b9fac180582bd
Blane <bbaumgartner21@gmail.com>
2018-03-02 09:41:11 -0500

    add bootstrap through node

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -200,6 +200,12 @@ class App extends Component {
               })}
             </div>
           </div>
+
+          <footer class="footer">
+            <div class="container">
+              <span class="text-muted">Place sticky footer content here.</span>
+            </div>
+          </footer>
         </div>
       );
     }
